e6f6f6edc0ec550672f4727cd04415a80bdd36ce
Sato Takumi <takumi07029@gmail.com>
2026-05-09 22:13:02 +0900

    initial commit: IONQ signal PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'ionq-signal-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// ネットワーク優先、失敗時にキャッシュを返す
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
